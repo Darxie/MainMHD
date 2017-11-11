fa2efdd8ece04a66e904add706668354ea95c117
Darxie <filip.hlavka@gmail.com>
2017-11-11 20:56:05 +0100

    change
    change

--- src/com/feldis/mhd/java/BusLines/Line30.java
+++ src/com/feldis/mhd/java/BusLines/Line30.java
@@ -15,7 +15,7 @@ public class Line30 extends BusLine{
 
     public Line30() {
         Line30 line30 = new Line30();
-        new CreateBusStops();
+        CreateBusStops.main(".");
         line30.createLine30(Gagarinova, Karadzicova, Mudronova, Botanicka, Slovnaftska);
 
         System.out.println(Arrays.toString(Line30.line.toArray()));
--- src/com/feldis/mhd/java/utils/CreateBusStops.java
+++ src/com/feldis/mhd/java/utils/CreateBusStops.java
@@ -11,27 +11,25 @@ import static com.feldis.mhd.java.utils.CalculateDistance.distance;
 
 public class CreateBusStops {
 
-    public static void CreateBusStops() {
+    public static void main(String args) {
 
-        BusStop Karadzicova = new BusStop(10,20, RandomizeNumberOfPeople.main(), "Karadzicova");
-        BusStop Slovnaftska = new BusStop(35,-40, RandomizeNumberOfPeople.main(), "Slovnaftska");
-        BusStop Botanicka = new BusStop(100,72, RandomizeNumberOfPeople.main(), "Botanicka");
-        BusStop Gagarinova = new BusStop(39,153, RandomizeNumberOfPeople.main(), "Gagarinova");
-        BusStop Tovarenska = new BusStop(-77,12, RandomizeNumberOfPeople.main(), "Tovarenska");
-        BusStop Mudronova = new BusStop(84,44, RandomizeNumberOfPeople.main(), "Mudronova");
-        BusStop Lafranconi = new BusStop(243,142, RandomizeNumberOfPeople.main(), "Lafranconi");
-        BusStop Kuliskova = new BusStop(96,22, RandomizeNumberOfPeople.main(), "Kuliskova");
+        BusStop Karadzicova = new BusStop(10, 20, RandomizeNumberOfPeople.main(), "Karadzicova");
+        BusStop Slovnaftska = new BusStop(35, -40, RandomizeNumberOfPeople.main(), "Slovnaftska");
+        BusStop Botanicka = new BusStop(100, 72, RandomizeNumberOfPeople.main(), "Botanicka");
+        BusStop Gagarinova = new BusStop(39, 153, RandomizeNumberOfPeople.main(), "Gagarinova");
+        BusStop Tovarenska = new BusStop(-77, 12, RandomizeNumberOfPeople.main(), "Tovarenska");
+        BusStop Mudronova = new BusStop(84, 44, RandomizeNumberOfPeople.main(), "Mudronova");
+        BusStop Lafranconi = new BusStop(243, 142, RandomizeNumberOfPeople.main(), "Lafranconi");
+        BusStop Kuliskova = new BusStop(96, 22, RandomizeNumberOfPeople.main(), "Kuliskova");
         BusStop Alexyho = new BusStop(2, 10, RandomizeNumberOfPeople.main(), "Alexyho");
-        BusStop Vysoka = new BusStop(-46,-23, RandomizeNumberOfPeople.main(), "Vysoka");
-        BusStop Postova = new BusStop(51,40, RandomizeNumberOfPeople.main(), "Postova");
-
-
+        BusStop Vysoka = new BusStop(-46, -23, RandomizeNumberOfPeople.main(), "Vysoka");
+        BusStop Postova = new BusStop(51, 40, RandomizeNumberOfPeople.main(), "Postova");
 
 
         //System.out.println(Lafranconi.posX + " " + Lafranconi.posY + " " + Lafranconi.nOfPeople);
         //System.out.println(Gagarinova.posX + " " + Gagarinova.posY + " " + Gagarinova.nOfPeople);
 
-        System.out.println(distance(Kuliskova.posX,Kuliskova.posY,Lafranconi.posX,Lafranconi.posY));
+        System.out.println(distance(Kuliskova.posX, Kuliskova.posY, Lafranconi.posX, Lafranconi.posY));
 
         //System.out.println(new Timer(Kuliskova,Lafranconi).time);
     }
